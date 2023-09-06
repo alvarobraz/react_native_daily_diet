7b0e765cb416222e3d85980385fd6cd32f896055
Álvaro Braz <alvarobraz83@gmail.com>
2023-09-06 13:28:43 -0300

    styled-components

---
+++ src/screens/Diet/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from './styles';
+
+export function Diet() {
+  return (
+    <Container>
+      <Title>
+        Dieta diária
+      </Title>
+    </Container>
+  );
+}
