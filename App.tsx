5f8f3124c5a459d86a0fca2309408464ccd1dfbe
Álvaro Braz <alvarobraz83@gmail.com>
2023-09-06 14:02:13 -0300

    custom fonts and loader and status bar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
 
 import theme from './src/@theme';
 
 import { Diet } from "./src/screens/Diet";
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Diet />
+      <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+      { fontsLoaded ? <Diet/> : <Loading /> }
     </ThemeProvider>
   );
 }
